476cbf2ba915eccc868c394ded85abdd33effd54
SkittlePox <bspiegel11@gmail.com>
2015-05-06 23:25:17 -0400

    Got Binary Searching working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     static binsearch bin = new binsearch();
-    static int[] i = {1,2,3};
+    static int[] i = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 
     public static void main(String[] args) {
-        System.out.println(bin.search(i)[1]);
+        System.out.println(bin.search(i));
     }
 }
--- src/com/company/binsearch.java
+++ src/com/company/binsearch.java
@@ -7,6 +7,46 @@ public class binsearch {
     binsearch() {
     }
 
-    public int search(int[] nums) {
-        
+    int length;
+
+    public double search(int[] nums) {
+        int upper = nums.length - 1, lower = 0, searchpt, count;
+        double avg = 0;
+        length = nums.length;
+        System.out.println("Length is " + length);
+        System.out.println("Upper is " + upper);
+        System.out.println("Lower is " + lower);
+        for (int x = 0; x < nums.length; x++) {
+            System.out.println("Testing element " + x);
+            count = 0;
+            lower = 0;
+            upper = nums.length - 1;
+
+            while (upper - lower >= 1) {
+                count++;
+                if ((upper - lower) % 2 == 0) {
+                    searchpt = (upper - lower) / 2 + lower;
+                } else {
+                    searchpt = (upper - lower + 1) / 2;
+                }
+
+                System.out.println("Searchpt is now " + searchpt);
+
+                if (searchpt + 1 == nums[x]) {
+                    System.out.println("Got it with " + count + " tries!");
+                    break;
+                } else if (searchpt + 1 < nums[x]) {
+                    lower = searchpt + 1;
+                    System.out.println("Element " + x + " is higher than searchpt");
+                } else {
+                    upper = searchpt - 1;
+                    System.out.println("Element " + x + " is lower than searchpt");
+                }
+                if (upper - lower <= 1) System.out.println("Got it with " + count + " tries!");
+            }
+            avg += count;
+        }
+        avg /= length;
+        return avg;
+    }
 }
