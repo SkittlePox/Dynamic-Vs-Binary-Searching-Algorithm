a7f9f9057f3a744a2bc2f35067b4df089715e47e
SkittlePox <bspiegel11@gmail.com>
2015-05-06 22:43:25 -0400

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    static binsearch bin = new binsearch();
+    static int[] i = {1,2,3};
+
+    public static void main(String[] args) {
+        System.out.println(bin.search(i)[1]);
+    }
+}
---
+++ src/com/company/binsearch.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Benjamin on 5/6/15.
+ */
+public class binsearch {
+    binsearch() {
+    }
+
+    public int search(int[] nums) {
+        
+}
