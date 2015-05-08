ccdd3183c97f97df3fee2e7d9ad46911e52590f4
SkittlePox <bspiegel11@gmail.com>
2015-05-07 21:51:31 -0400

    Added graphs with Binary and Dynamic Search averages.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,57 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    int size = 100;
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Binary VS. Dynamic Searching");
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Length of Array");
+        yAxis.setLabel("Average Number of Tries");
+        //creating the chart
+        final LineChart<Number, Number> lineChart =
+                new LineChart<Number, Number>(xAxis, yAxis);
+
+        lineChart.setTitle("Average Number of Tries Per Algorithm");
+        lineChart.setCreateSymbols(false);
+        //defining a series
+        XYChart.Series bins = new XYChart.Series();
+        XYChart.Series dyns = new XYChart.Series();
+
+        bins.setName("Binary Search");
+        dyns.setName("Dynamic Search");
+
+        //populating the series with data
+        for (int i = 1; i <= size; i++) {
+            bins.getData().add(new XYChart.Data(i, bin.search(makeArray(i))));
+            dyns.getData().add(new XYChart.Data(i, dyn.search(makeArray(i))));
+        }
+
+        Scene scene = new Scene(lineChart, 1000, 700);
+        lineChart.getData().add(bins);
+        lineChart.getData().add(dyns);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
     static binsearch bin = new binsearch();
+    static dynsearch dyn = new dynsearch();
 
     public static void main(String[] args) {
-        int points = 1000;
-        System.out.println(bin.search(makeArray(1000000)));
-//        double[] Binary_Average = new double[points];
-//        for (int x = 1; x <= points; x++) {
-//            Binary_Average[x - 1] = bin.search(makeArray(x));
-//        }
+        launch(args);
+//        System.out.println(dyn.search(makeArray(100)));
     }
 
     public static int[] makeArray(int length) {
---
+++ src/com/company/dynsearch.java
@@ -0,0 +1,59 @@
+package com.company;
+
+/**
+ * Created by Benjamin on 5/7/15.
+ */
+public class dynsearch {
+    dynsearch() {
+    }
+
+    int length;
+
+    public double search(int[] nums) {
+        int upper = nums.length - 1, lower = 0, searchpt, count;
+        double avg = 0;
+        length = nums.length;
+        System.out.println("Length is " + length);
+        System.out.println("Upper is " + upper);
+        System.out.println("Lower is " + lower);
+        for (int x = 0; x < nums.length; x++) {
+            System.out.println("Testing element " + x);
+            count = 0;
+            lower = 0;
+            upper = nums.length - 1;
+
+            while (upper - lower >= 1) {
+                count++;
+
+                if ((upper - lower + 1) % 4 == 0) {
+                    searchpt = ((upper - lower + 1) / 4) * 3 - 1 + lower;
+                } else if ((upper - lower + 1) % 2 == 0) {
+                    searchpt = (((upper - lower + 1) - 2) / 4) * 3 + lower;
+                } else if ((upper - lower) % 4 == 0) {
+                    searchpt = ((upper - lower) / 4) * 3 + lower;
+                } else {
+                    searchpt = (((upper - lower) - 2) / 4) * 3 + lower + 1;
+                }
+
+                System.out.println("Searchpt is now " + searchpt);
+
+                if (searchpt + 1 == nums[x]) {
+                    System.out.println("Got it with " + count + " tries!");
+                    break;
+                } else if (searchpt + 1 < nums[x]) {
+                    lower = searchpt + 1;
+                    System.out.println("Element " + x + " is higher than searchpt");
+                } else {
+                    upper = searchpt - 1;
+                    System.out.println("Element " + x + " is lower than searchpt");
+                }
+                System.out.println("Upper is " + upper);
+                System.out.println("Lower is " + lower);
+                if (upper - lower <= 1) System.out.println("Got it with " + count + " tries!");
+            }
+            avg += count;
+        }
+        avg /= length;
+        return avg;
+    }
+}
